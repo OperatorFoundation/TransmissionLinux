6c30bd4b51d7e31eea128bcbc732aeb26f353ecd
CryptoSax <joshua@operatorfoundation.org>
2021-11-19 15:35:53 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/Chord", from: "0.0.15"),
         .package(url: "https://github.com/OperatorFoundation/Datable", from: "3.1.2"),
         .package(name: "Socket", url: "https://github.com/OperatorFoundation/BlueSocket", from: "1.1.0"),
-        .package(url: "https://github.com/OperatorFoundation/Net", from: "0.0.1"),
-        .package(url: "https://github.com/OperatorFoundation/Transport", from: "2.3.9"),
+        .package(url: "https://github.com/OperatorFoundation/Net", from: "1.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/Transport", from: "2.3.10"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2")
     ],
     targets: [
