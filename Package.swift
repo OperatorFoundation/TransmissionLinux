d5033655371241024cb27565f86cf7a82da55769
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2021-12-08 13:43:34 -0600

    Switched dependencies to tracking branches

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/TransmissionTypes", from: "0.0.1"),
-        .package(url: "https://github.com/OperatorFoundation/Chord", from: "0.0.15"),
-        .package(url: "https://github.com/OperatorFoundation/Datable", from: "3.1.4"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionTypes", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Chord", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
         .package(name: "Socket", url: "https://github.com/Kitura/BlueSocket", from: "2.0.2"),
-        .package(url: "https://github.com/OperatorFoundation/Net", from: "0.0.6"),
+        .package(url: "https://github.com/OperatorFoundation/Net", branch: "main"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
-        .package(url: "https://github.com/OperatorFoundation/SwiftHexTools.git", from: "1.2.5")
+        .package(url: "https://github.com/OperatorFoundation/SwiftHexTools.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
