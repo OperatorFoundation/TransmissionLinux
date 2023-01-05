5f179ed4a57ab6cab9b9179757a8ed88115aee06
CryptoSax <joshua@operatorfoundation.org>
2023-01-05 14:33:20 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "TransmissionLinux",
     platforms: [
         .macOS(.v12),
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
