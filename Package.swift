d4c40433f05853c00bbf3718cf14282a91175cd9
consuelita <adelita@operatorfoundation.org>
2024-02-18 13:18:02 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,14 +16,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
-        .package(url: "https://github.com/OperatorFoundation/BlueSocket", branch: "1.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/Chord", branch: "0.1.1"),
+        .package(url: "https://github.com/OperatorFoundation/BlueSocket", from: "1.1.2"),
+        .package(url: "https://github.com/OperatorFoundation/Chord", from: "0.1.4"),
         .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/Net", branch: "release"),
-        .package(url: "https://github.com/OperatorFoundation/Straw", branch: "1.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/Net", from: "0.0.10"),
+        .package(url: "https://github.com/OperatorFoundation/Straw", from: "1.0.1"),
         .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", from: "1.2.6"),
-        .package(url: "https://github.com/OperatorFoundation/TransmissionBase", branch: "release"),
-        .package(url: "https://github.com/OperatorFoundation/TransmissionTypes", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionBase", from: "1.0.1"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionTypes", from: "0.0.2"),
     ],
     targets: [
         .target(
