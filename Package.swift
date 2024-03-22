5a139c79041377b0c11088397045bf03e9bb1a9c
bragelbytes <joebragel@gmail.com>
2024-03-22 13:42:56 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,12 +17,12 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
         .package(url: "https://github.com/OperatorFoundation/BlueSocket", from: "1.1.2"),
-        .package(url: "https://github.com/OperatorFoundation/Chord", from: "0.1.4"),
-        .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/Chord", from: "0.1.5"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.1"),
         .package(url: "https://github.com/OperatorFoundation/Net", from: "0.0.10"),
-        .package(url: "https://github.com/OperatorFoundation/Straw", from: "1.0.1"),
+        .package(url: "https://github.com/OperatorFoundation/Straw", from: "1.0.4"),
         .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", from: "1.2.6"),
-        .package(url: "https://github.com/OperatorFoundation/TransmissionBase", from: "1.0.1"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionBase", from: "1.0.2"),
         .package(url: "https://github.com/OperatorFoundation/TransmissionTypes", from: "0.0.2"),
     ],
     targets: [
